0457c69dee668810d9835ff0baa9a91080ebf4dc
MohammadEbrahimi <mohammad.ebrahimi61173@gmail.com>
2022-08-08 17:19:59 +0430

    versioning

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  entry: './index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'image.bundle.js',
   },
   module: {
-    rules: [{ test: /\.txt$/, use: 'raw-loader' }],
+    rules: [
+      { test: /\.ts$/, use: 'ts-loader' },
+    ],
   },
 };
